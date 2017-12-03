c3e83e24f7598d7019ab36302080a07042800891
jesusjimsa <jesusjimsa@icloud.com>
2017-12-03 11:53:07 +0200

    First detection method

--- Lab 1/src/skinDetection.cpp
+++ Lab 1/src/skinDetection.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>
 #include <opencv2/core/core.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
@@ -15,8 +16,45 @@
 using namespace std;
 using namespace cv;
 
+int max(int first, int second, int third){
+	int max = first;
+
+	if(second > max){
+		max = second;
+	}
+
+	if(third > max){
+		max = third;
+	}
+
+	return max;
+}
+
+int min(int first, int second, int third){
+	int min = first;
+
+	if(second < min){
+		min = second;
+	}
+
+	if(third < min){
+		min = third;
+	}
+
+	return min;
+}
+
+bool Detection1(Vec3b color){
+	bool part1 = (int)color[0] > 20 && (int)color[1] > 40 && (int)color[2] > 95;
+	bool part2 = (max((int)color[0], (int)color[1], (int)color[2]) - min((int)color[0], (int)color[1], (int)color[2])) > 15;
+	bool part3 = abs((int)color[2] - (int)color[1]) > 15 && (int)color[2] > (int)color[1] && (int)color[2] > (int)color[0];
+
+	return part1 && part2 && part3;
+}
+
 int main(){
 	Mat img[12];
+	Vec3b color;
 	string windowName;
 
 	// Open the images
@@ -37,6 +75,27 @@ int main(){
 	for(int i = 0; i < 12; i++){
 		if(!img[i].data){
 			cout << "Image " << i << " could not be opened" << endl;
+		}
+	}
+
+	for(int i = 0; i < 12; i++){
+		for(int y = 0; y < img[i].rows; y++){
+			for(int x = 0; x < img[i].cols; x++){
+				color = img[i].at<Vec3b>(Point(x, y));
+
+				/*
+					0 --> B
+					1 --> G
+					2 --> R
+				*/
+				if(Detection1(color)){
+					color[0] = 0;
+					color[1] = 0;
+					color[2] = 255;
+				}
+
+				img[i].at<Vec3b>(Point(x, y)) = color;
+			}
 		}
 	}
 
