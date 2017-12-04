90d3ea61e870a361ee78577276eeb2c27bed32e6
jesusjimsa <jesusjimsa@icloud.com>
2017-12-04 13:17:51 +0200

    Emoji created

--- Lab 1/emoji/emoji.cpp
+++ Lab 1/emoji/emoji.cpp
@@ -13,17 +13,38 @@ using namespace std;
 using namespace cv;
 
 int main(){
-	// To create an image
-    // CV_8UC3 depicts : (3 channels,8 bit image depth
-    // Height  = 500 pixels, Width = 1000 pixels
-    // (0, 0, 100) assigned for Blue, Green and Red 
-    //             plane respectively. 
-    // So the image will appear red as the red 
-    // component is set to 100.
     Mat img(500, 500, CV_8UC3, Scalar(255,255, 255));
 
+	// Shape of the face
+	circle(img, Point(249, 249), 200.0, Scalar(0, 255, 255), -1, CV_AA);
+	circle(img, Point(249, 249), 200.0, Scalar(0, 0, 0), 1, CV_AA);
+	
+	// Eyes
+	//// Left eye
+	circle(img, Point(170, 180), 50.0, Scalar(255, 255, 255), -1, CV_AA);
+	circle(img, Point(170, 180), 50.0, Scalar(0, 0, 0), 1, CV_AA);
+	circle(img, Point(170, 180), 10.0, Scalar(0, 0, 0), -1, CV_AA);
+
+	//// Right eye
+	circle(img, Point(328, 180), 50.0, Scalar(255, 255, 255), -1, CV_AA);
+	circle(img, Point(328, 180), 50.0, Scalar(0, 0, 0), 1, CV_AA);
+	circle(img, Point(328, 180), 10.0, Scalar(0, 0, 0), -1, CV_AA);
+
+	// Mouth
+	ellipse(img, Point(249, 350), Size(40.0, 70.0), 90, 0, 360, Scalar(0, 0, 0), -1, CV_AA);
+	// line(img, Point(200, 370), Point(298, 370), Scalar(0, 0, 0), 3, CV_AA);
+
+	// Rouge
+	// circle(img, Point(140, 310), 40.0, Scalar(132, 17, 255), -1, CV_AA);
+	// circle(img, Point(358, 310), 40.0, Scalar(132, 17, 255), -1, CV_AA);
+
+	// line(img, Point(249, 0), Point(249, 499), Scalar(0, 0, 0), 1, CV_AA);
+	// line(img, Point(0, 249), Point(499, 249), Scalar(0, 0, 0), 1, CV_AA);
+
+	// Display image (and save)
 	namedWindow("emoji", WINDOW_AUTOSIZE);	// Create a window for display.
 	imshow("emoji", img);				// Show our image inside it.
+	imwrite("emoji.png", img);			// Save image
 
 	
 	waitKey(0);									// Wait for a keystroke in the window
