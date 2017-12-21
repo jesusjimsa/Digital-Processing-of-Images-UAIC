c956a4a6f0dd89d8ce8682f495a4d5f807636ebd
jesusjimsa <jesusjimsa@icloud.com>
2017-12-22 01:31:37 +0200

    Face detection

--- Lab 1/skinDetection/skinDetection.cpp
+++ Lab 1/skinDetection/skinDetection.cpp
@@ -13,9 +13,17 @@
 #include <opencv2/core/core.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
 
 using namespace std;
 using namespace cv;
+
+// Global variables
+string face_cascade_name = "../../haarcascade_frontalface_alt.xml";
+CascadeClassifier face_cascade;
+string window_name = "Capture - Face detection";
+int filenumber; // Number of file to be saved
+string filename;
 
 /*
 	Functions to get max and min values
@@ -190,10 +198,98 @@ bool Detection7(Vec3b color){
 	return part1 && part2;
 }
 
+// Function detectAndDisplay
+void detectAndDisplay(Mat frame){
+	vector<Rect> faces;
+	Mat frame_gray;
+	Mat crop;
+	Mat res;
+	Mat gray;
+	string text;
+	stringstream sstm;
+
+	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+
+	// Detect faces
+	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+
+	// Set Region of Interest
+	Rect roi_b;
+	Rect roi_c;
+
+	size_t ic = 0; // ic is index of current element
+	int ac = 0; // ac is area of current element
+
+	size_t ib = 0; // ib is index of biggest element
+	int ab = 0; // ab is area of biggest element
+
+	for (ic = 0; ic < faces.size(); ic++){ // Iterate through all current elements (detected faces){
+		roi_c.x = faces[ic].x;
+		roi_c.y = faces[ic].y;
+		roi_c.width = (faces[ic].width);
+		roi_c.height = (faces[ic].height);
+
+		ac = roi_c.width * roi_c.height; // Get the area of current element (detected face)
+
+		roi_b.x = faces[ib].x;
+		roi_b.y = faces[ib].y;
+		roi_b.width = (faces[ib].width);
+		roi_b.height = (faces[ib].height);
+
+		ab = roi_b.width * roi_b.height; // Get the area of biggest element, at beginning it is same as "current" element
+
+		if(ac > ab){
+			ib = ic;
+			roi_b.x = faces[ib].x;
+			roi_b.y = faces[ib].y;
+			roi_b.width = (faces[ib].width);
+			roi_b.height = (faces[ib].height);
+		}
+
+		crop = frame(roi_b);
+		resize(crop, res, Size(128, 128), 0, 0, INTER_LINEAR); // This will be needed later while saving images
+		cvtColor(crop, gray, CV_BGR2GRAY); // Convert cropped image to Grayscale
+
+		// Form a filename
+		// filename = "";
+		// stringstream ssfn;
+		// ssfn << filenumber << ".png";
+		// filename = ssfn.str();
+		// filenumber++;
+
+		// imwrite(filename, gray);
+
+		Point pt1(faces[ic].x, faces[ic].y);	// Display detected faces on main window - live stream from camera
+		Point pt2((faces[ic].x + faces[ic].height), (faces[ic].y + faces[ic].width));
+		rectangle(frame, pt1, pt2, Scalar(0, 255, 0), 2, 8, 0);
+	}
+
+	// Show image
+	// sstm << "Crop area size: " << roi_b.width << "x" << roi_b.height << " Filename: " << filename;
+	// text = sstm.str();
+
+	//putText(frame, text, cvPoint(30, 30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(0, 0, 255), 1, CV_AA);
+	imshow("original", frame);
+
+	if(!crop.empty()){
+		imshow("detected", crop);
+	}
+	else{
+		destroyWindow("detected");
+	}
+}
+
 int main(){
 	Mat img[12];
 	Vec3b color;
 	string windowName;
+
+	// Load the cascade
+    if (!face_cascade.load(face_cascade_name)){
+        printf("--(!)Error loading\n");
+        return (-1);
+    }
 
 	// Open the images
 	img[0] = imread("/Users/jesusjimsa/Dropbox/Documentos/Universidad/3 - Primer cuatrimestre/Digital Processing of Images/Prácticas/Digital-Processing-of-Images-UAIC/Lab 1/Images/1.jpg");
@@ -213,6 +309,15 @@ int main(){
 	for(int i = 0; i < 12; i++){
 		if(!img[i].data){
 			cout << "Image " << i << " could not be opened" << endl;
+		}
+
+		// Apply the classifier to the frame
+		if (!img[i].empty()){
+			detectAndDisplay(img[i]);
+		}
+		else{
+			printf(" --(!) No captured frame -- Break!");
+			break;
 		}
 	}
 
@@ -243,6 +348,12 @@ int main(){
 		namedWindow(windowName, WINDOW_AUTOSIZE);	// Create a window for display.
 		imshow(windowName, img[i]);				// Show our image inside it.
 	}
+
+	// int c = waitKey(10);
+
+	// if (27 == char(c)){
+	// 	;
+	// }
 	
 	waitKey(0);									// Wait for a keystroke in the window
 }
